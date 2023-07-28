e0c05a3a3a427f62e46ba2c4b9472b5d2974b8a6
neverloved-dev <djordjeandjic99@gmail.com>
2023-07-28 22:22:56 +0200

    add views

--- backend/dicts/views.py
+++ backend/dicts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from dicts.models import *
+from dicts.serializers import *
 
-# Create your views here.
+class SpecialityViewSet(ModelViewSet):
+	queryset = Speciality.objects.all()
+	serializer_class = SpecialtySerializer
+
+class UgsnViewSet(ModelViewSet):
+		queryset = Ugsn.objects.all()
+		serializer_class = UgsnSerializer
+
+class CompetenceViewSet(ModelViewSet):
+	queryset = Competence.objects.all()
+	serializer_class = CompetenceSerializer
+
+class IndicatorViewSet(ModelViewSet):
+	queryset = Indicator.objects.all()
+	serializer_class = CompetenceSerializer
+
+class SubjectViewSet(ModelViewSet):
+	queryset = Subject.objects.all()
+	serializer_class = CompetenceSerializer
+
+class EducationProgramViewSet(ModelViewSet):
+	queryset = EducationProgram.objects.all()
+	serializer_class = EducationProgramSerializer
