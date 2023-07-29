3d80973c212b86999a9b82eebc6829f948953936
Vovan <vovanex12@gmail.com>
2023-07-29 17:26:20 +0300

    Автоматическое создание сущностей user_app работает

--- backend/initers/components/user_app/main.py
+++ backend/initers/components/user_app/main.py
@@ -1,9 +1,10 @@
+import random
 from django.contrib.auth import get_user_model
 
-from dicts.models import EducationProgram
+from dicts.models import EducationProgram, Subject
 from initers.base import IniterComposite, Initer
 from initers.components.user_app.users import UsersIniter
-from user_app.models import Group
+from user_app.models import Group, Teacher, GroupSubject
 
 User = get_user_model()
 
@@ -25,14 +26,22 @@ class GroupIniter(Initer):
             Group.objects.create(name=args[0], education_program=education_program)
 
 
-# class GroupSubjectIniter(Initer):
-#     fixtures = (
-#         ("ТЛ-121")
-#     )
-#
-#     @classmethod
-#     def start(cls):
-#         pass
+class GroupSubjectIniter(Initer):
+    @classmethod
+    def start(cls):
+        teachers = Teacher.objects.all()
+        subjects = Subject.objects.all()
+
+        for group_args in GroupIniter.fixtures:
+            group = Group.objects.get(name=group_args[0])
+            group_subjects = random.choices(subjects, k=random.randint(3, 5))
+
+            for group_subject in group_subjects:
+                group_teacher = random.choice(teachers)
+                GroupSubject.objects.create(
+                    group=group, subject=group_subject, teacher=group_teacher
+                )
+
 
 class MainIniter(IniterComposite):
-    initers = GroupIniter, UsersIniter
+    initers = GroupIniter, UsersIniter, GroupSubjectIniter
