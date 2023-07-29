2013078a1d2d3e9b147e1da041d2c5b3606a1568
Vovan <vovanex12@gmail.com>
2023-07-29 13:15:37 +0300

    Добавил дополнительные модели. Внимание миграции изменены!

--- backend/user_app/admin.py
+++ backend/user_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Student, Teacher, Group, GroupSubject
+
+for model in [
+    Student, Teacher, Group, GroupSubject, User
+]:
+    admin.site.register(model)
--- backend/user_app/models.py
+++ backend/user_app/models.py
@@ -8,10 +8,19 @@ class User(AbstractUser):
     is_student = models.BooleanField(default=False, verbose_name="Статус студента")
     is_teacher = models.BooleanField(default=False, verbose_name="Статус преподавателя")
 
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return f"{self.last_name} {self.first_name}"
+        else:
+            return str(self.username)
+
 
 class Group(models.Model):
     name = models.CharField("Название", max_length=20, unique=True)
     education_program = models.ForeignKey(EducationProgram, on_delete=models.PROTECT, verbose_name="Программа обучения")
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Учебная группа'
@@ -19,9 +28,12 @@ class Group(models.Model):
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name="Данные о пользователе")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name="Пользователь")
     group = models.ForeignKey(Group, on_delete=models.PROTECT, verbose_name="Учебная группа")
     markbook_id = models.CharField(max_length=30, verbose_name="Номер зачетной книжки")
+
+    def __str__(self):
+        return f"{self.group} {self.user}"
 
     class Meta:
         verbose_name = "Студент"
@@ -29,15 +41,27 @@ class Student(models.Model):
 
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name="Пользователь")
+
+    def __str__(self):
+        return str(self.user)
 
     class Meta:
         verbose_name = "Преподаватель"
         verbose_name_plural = "Преподаватели"
 
 
-# Проверить на совпадение компетенции преподавателя, дисциплины и программы группы
-class SubjectF(models.Model):
+# todo Проверить на совпадение компетенции преподавателя, дисциплины и программы группы
+class GroupSubject(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Учебная группа")
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name="Дисциплина")
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name="Преподаватель")
+
+    def __str__(self):
+        return f"{self.group} {self.subject}"
+
+    class Meta:
+        verbose_name = "Предмет, изучаемый группой"
+        verbose_name_plural = "Предметы, изучаемые группами"
+
+
