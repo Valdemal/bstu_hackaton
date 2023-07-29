10e5675d02a1d20145378ee07961b440fd24bb06
Vovan <vovanex12@gmail.com>
2023-07-29 22:25:46 +0300

    Расширил сериализаторы для Student и Teacher, добавил действие для получения списка студентов группы

--- backend/user_app/serializers.py
+++ backend/user_app/serializers.py
@@ -5,23 +5,37 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher')
-        read_only_fields = ('id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher')
+        fields = ('id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher', 'first_name', 'last_name')
+        read_only_fields = (
+            'id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher', 'first_name', 'last_name'
+        )
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class RelatedStudentSerializers(serializers.PrimaryKeyRelatedField, StudentSerializer):
+    pass
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
 
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'
 
 class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Teacher
         fields = '__all__'
+
 
 class GroupSubjectSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
 from user_app.models import *
 from user_app.serializers import GroupSerializer, GroupSubjectSerializer, StudentSerializer, TeacherSerializer
-# Create your views here.
+
 
 class GroupViewSet(ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+    @extend_schema(responses=StudentSerializer(many=True), description="Возвращает список студентов группы")
+    @action(methods=['GET'], detail=True)
+    def students(self, request, pk):
+        serialized = StudentSerializer(Student.objects.filter(group__pk=pk), many=True)
+        return Response(serialized.data)
+
+
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
 class TeacherViewSet(ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+
 
 class GroupSubjectViewSet(ModelViewSet):
     queryset = GroupSubject.objects.all()
