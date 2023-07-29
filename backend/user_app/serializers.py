2b1bcedd5693edf30c65cd8893962300b50d3aa0
neverloved-dev <djordjeandjic99@gmail.com>
2023-07-29 20:40:18 +0200

    additional api logic

--- backend/user_app/serializers.py
+++ backend/user_app/serializers.py
@@ -1,4 +1,4 @@
-from user_app.models import User
+from user_app.models import *
 from rest_framework import serializers
 
 
@@ -7,3 +7,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher')
         read_only_fields = ('id', 'username', 'email', 'is_staff', 'is_student', 'is_teacher')
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class GroupSubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupSubject
+        fields = '__all__'
--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from user_app.models import *
+from user_app.serializers import GroupSerializer, GroupSubjectSerializer, StudentSerializer, TeacherSerializer
 # Create your views here.
+
+class GroupViewSet(ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherViewSet(ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class GroupSubjectViewSet(ModelViewSet):
+    queryset = GroupSubject.objects.all()
+    serializer_class = GroupSubjectSerializer
