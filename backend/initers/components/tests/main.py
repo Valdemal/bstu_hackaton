0602b5390360a05af7afae18f591467c6b4cef5b
Vovan <vovanex12@gmail.com>
2023-07-31 01:10:40 +0300

    Дополнил инитеры для приложения testing

--- backend/initers/components/tests/main.py
+++ backend/initers/components/tests/main.py
@@ -1,4 +1,5 @@
 ﻿import random
+from datetime import datetime, timedelta
 
 from initers.base import IniterComposite, Initer
 from testing.models import *
@@ -7,20 +8,18 @@ from user_app.models import *
 
 
 class TestIniter(Initer):
-
     @classmethod
     def start(cls):
         for teacher in Teacher.objects.all():
-            avaliable_group_subjects = GroupSubject.objects.filter(teacher=teacher)
+            available_group_subjects = GroupSubject.objects.filter(teacher=teacher)
             for i in range(random.randint(3, 5)):
                 name = "Тест " + str(random.randint(1, 100))
 
-                subject = random.choice(avaliable_group_subjects).subject
+                subject = random.choice(available_group_subjects).subject
                 competence = subject.competences.first()
-                avaliable_indicators = list(Indicator.objects.filter(competence=competence))
-                # todo пофиксить
+                available_indicators = list(Indicator.objects.filter(competence=competence))
                 indicators = random.sample(
-                    avaliable_indicators, len(avaliable_indicators)
+                    available_indicators, len(available_indicators)
                 )
 
                 test = Test.objects.create(name=name, teacher=teacher, subjects=subject)
@@ -30,46 +29,43 @@ class TestIniter(Initer):
 
 
 class QuestionIniter(Initer):
-    fixtures = (
-        "Вопрос 1",
-        "Вопрос 2",
-        "Вопрос 3",
-        "Вопрос 4",
-        "Вопрос 5",
-    )
+    QUESTIONS_COUNT = 5
 
     @classmethod
     def start(cls):
-        pass
-#         tests = Test.objects.all()
-#         for args in cls.fixtures:
-#             test = random.choice(tests)
-#             correct_answer = random.choice(Answer.objects.all())
-#             Question.objects.create(test=test, correct_answer=correct_answer, text=args)
-
-
-class AnswerIniter(Initer):
-    fixtures = (
-        "Ответ 1",
-        "Ответ 2",
-        "Ответ 3",
-        "Ответ 4",
-        "Ответ 5",
-    )
-
-    @classmethod
-    def start(cls):
-        pass
+        for test in Test.objects.all():
+            for i in range(cls.QUESTIONS_COUNT):
+                text = "Пример вопроса №" + str(i+1) + " к тесту "
+                correct_answer = "Пример правильного ответа на " + text
+                Question.objects.create(test=test, correct_answer=correct_answer, text=text)
 
 
 class AssignedTestIniter(Initer):
-    fixtures = (
-    )
-
     @classmethod
     def start(cls):
-        pass
+        for test in Test.objects.all():
+            available_students = list(Student.objects.filter(group__groupsubject__teacher=test.teacher).distinct())
+            end_time = datetime.now() + timedelta(days=1)
+
+            for student in random.sample(available_students, random.randint(1, len(available_students))):
+                AssignedTest.objects.create(test=test, student=student, end_time=end_time)
+
+
+class AnswerIniter(Initer):
+    @classmethod
+    def start(cls):
+        for assigned_test in AssignedTest.objects.all():
+            questions = Question.objects.filter(test=assigned_test.test)
+            for question in questions:
+                is_correct_answer = bool(random.randint(0, 1))\
+
+                if is_correct_answer:
+                    answer_text = question.correct_answer
+                else:
+                    answer_text = f"Неправильный ответ на вопрос {question}"
+
+                Answer.objects.create(question=question, answer=answer_text)
 
 
 class MainIniter(IniterComposite):
-    initers = TestIniter, QuestionIniter, AnswerIniter, AssignedTestIniter
+    initers = TestIniter, QuestionIniter, AssignedTestIniter, AnswerIniter
