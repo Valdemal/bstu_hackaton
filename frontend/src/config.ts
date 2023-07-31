f61e4e8b84bd14238d467d4adc454e4be5b45d60
Vovan <vovanex12@gmail.com>
2023-07-31 05:27:12 +0300

    Версия для деплоя на хост

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_PROTOCOL = 'http';
-export const BACKEND_HOST     = 'localhost';
-export const BACKEND_PORT     = '80';
+export const BACKEND_HOST     = '77.222.55.58';
+export const BACKEND_PORT     = '81';
